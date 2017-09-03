bebd2cc1d5a8d61b1933c810687ef9176b92aec0
Jan Kowalski <luca.r@wp.pl>
2017-09-03 13:29:29 +0200

    q

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Dodaj do siebie dwie losowe liczby");
+
+
         }
     }
 }
